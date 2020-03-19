9bbed49178c2512e6a751215e9a2d6f64c6bb7d0
Jörg B <34109209+jbar98@users.noreply.github.com>
2020-03-19 23:58:57 +0100

    Create keyboard abstraction

---
+++ src/keyboard/Keyboard.hpp
@@ -0,0 +1,13 @@
+#include <map>
+
+class Keyboard
+{
+public:
+    Keyboard keyboard();
+
+    // implement some routine which converts
+    // the default mapping to a user-defined mapping
+private:  
+
+    std::map<int, int> remap;
+};
