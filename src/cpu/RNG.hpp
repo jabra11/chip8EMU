16ce893f62e772af9d86053a50d4545a0e744800
Jörg B <34109209+jbar98@users.noreply.github.com>
2020-03-24 21:53:04 +0100

    Create random number generator for the CPU

---
+++ src/cpu/RNG.hpp
@@ -0,0 +1,15 @@
+#include <random>
+
+class RNG
+{
+public:
+    // inclusive range
+    int get(const int min, const int max)
+    {
+        std::uniform_int_distribution<> dis(min, max);
+        return dis(gen);
+    }
+
+private:
+    std::mt19937 gen{std::random_device{}()};
+};
