b9b991fdfde73df8d40dac03c7373535158b2270
Jörg B <34109209+jbar98@users.noreply.github.com>
2020-03-24 21:52:49 +0100

    Place special registers in public accessibility

--- src/cpu/Registers.hpp
+++ src/cpu/Registers.hpp
@@ -9,21 +9,23 @@ public:
     uint8_t get(const uint8_t reg) const;
     void put(const uint8_t reg, const uint8_t val);
 
-private:
-    // general purpose 8bit registers
-    // V[0x000F] should not be used by any program. It
-    // is used by some instructions to set flags.
-    std::array<uint8_t, 16> V{};
-
     // I register is used to store memory addresses
     uint16_t I;
-
+    
     // special purpose register, when these are != 0
     // they should be decremented at a rate of 60HZ.
     // sound-timer
     uint8_t ST;
     // delay-timer
     uint8_t DT;
+private:
+
+    // general purpose 8bit registers
+    // V[0x000F] should not be used by any program. It
+    // is used by some instructions to set flags.
+    std::array<uint8_t, 16> V{};
+
+
 };
 
 #endif // REGISTERS_HPP
