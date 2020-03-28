f49ef0da95e93f40f08b95ba8364825760e537c2
Jörg B <34109209+jbar98@users.noreply.github.com>
2020-03-28 22:19:33 +0100

    Implement some emulation delay and implement very basic graphics

--- src/Emulator.cpp
+++ src/Emulator.cpp
@@ -1,13 +1,79 @@
 #include <vector>
 #include <fstream>
 #include <iostream>
+#include <chrono> // sleep_for helper
+#include <thread> // sleep_for
+
+#include <cstddef>
+
+std::byte b;
+
+#include <SFML/Graphics.hpp>
 
 #include "Emulator.hpp"
+#include "cpu/Timer.hpp"
+#include "Object_manager.hpp"
+
+Emulator::Emulator(int w, int h)
+    :window{sf::VideoMode(w, h), "Chip8EMU"}
+{
+    using P = Object_manager::Object_property;
+
+    // add program counter
+    P tmp;
+    tmp.name = "PC";
+    tmp.pos = sf::Vector2f(w/2, h/2);
+    tmp.color = sf::Color::Green;
+    manager.add_text(tmp);
+
+    // add opcode
+    tmp = P{};
+    tmp.name = "Instruction";
+    tmp.color = sf::Color::Green;
+    tmp.pos = sf::Vector2f(w / 3, h/10 * 1);
+    manager.add_text(tmp);
+
+    // add registers
+    for (int i = 0; i < 16; ++i)
+    {
+        P tmp;
+        tmp.name = "V" + std::to_string(i);
+        tmp.color = sf::Color::Green;
+        tmp.pos = sf::Vector2f(w / 10 * 7, h / 20 * i + 50);
+
+        manager.add_text(tmp);
+    }
+
+    // add background
+    tmp = P{};
+    tmp.name = "background";
+    tmp.dim = sf::Vector2f(w, h);
+
+    manager.add_rectangle(tmp);
+}
 
 void Emulator::run()
 {
-    while (true)
-        cpu.execute_next_cycle();
+    std::chrono::steady_clock tim;
+    auto point = tim.now();
+    while (window.isOpen())
+    {
+        handle_events();
+        emulate_cpu();
+
+        if(timer.is_ready(Timer::Type::sound, 144))
+        {
+            update_graphics();
+            render();
+        }
+        if (std::chrono::duration_cast<std::chrono::milliseconds>
+            (tim.now() - point).count() > 1000)
+        {
+            std::cout << clock_cycles_done << std::endl;
+            clock_cycles_done = 0;
+            point = tim.now();
+        }
+    }
 }
 
 void Emulator::inject_rom(const std::string &path, int start_index)
@@ -16,6 +82,68 @@ void Emulator::inject_rom(const std::string &path, int start_index)
 
     for (size_t i = 0; i < bytes.size(); ++i) 
         ram.write(start_index + i, bytes[i]);
+}
+   
+void Emulator::handle_events()
+{
+    sf::Event evt;
+    while (window.pollEvent(evt))
+    {
+        switch (evt.type)
+        {
+            case sf::Event::Closed: window.close(); return;
+            default: return;
+        }
+    }
+}
+
+void Emulator::render()
+{
+    window.clear();
+
+    window.draw(manager.get_rectangle_ref("background"));
+    window.draw(manager.get_text_ref("PC"));
+    window.draw(manager.get_text_ref("Instruction"));
+    
+    for(int i = 0; i < 16; ++i)
+        window.draw(manager.get_text_ref("V" + std::to_string(i)));
+
+    window.display();
+}
+
+void Emulator::update_graphics()
+{
+    manager.get_text_ref("PC").setString("PC: " 
+            + std::to_string(cpu.get_pc()));
+    
+    manager.get_text_ref("Instruction").setString("Current opcode: " + 
+            cpu.get_current_opcode());
+    for (int i = 0; i < 9; ++i)
+    {
+        manager.get_text_ref("V" + std::to_string(i)).setString("V0"
+            + std::to_string(i) + ": " + std::to_string(cpu.get_reg_at(i)));
+    }
+    for (int i = 9; i < 16; ++i)
+    {
+        manager.get_text_ref("V" + std::to_string(i)).setString("V"
+            + std::to_string(i) + ": " + std::to_string(cpu.get_reg_at(i)));
+    }
+}
+
+void Emulator::emulate_cpu()
+{
+    if (cpu_ready())
+    {
+        cpu.execute_next_cycle();
+        ++clock_cycles_done;
+    }
+}
+
+bool Emulator::cpu_ready()
+{
+    if (timer.is_ready(Timer::Type::delay, clock_speed))
+        return true;
+    return false;
 }
    
 std::vector<uint8_t> Emulator::get_bytes(const std::string& path)
--- src/Emulator.hpp
+++ src/Emulator.hpp
@@ -2,24 +2,48 @@
 #define EMULATOR_HPP
 
 #include <vector>
+#include <SFML/Graphics.hpp>
 
 #include "cpu/CPU.hpp"
 #include "memory/Memory.hpp"
+#include "display/Display.hpp"
+#include "keyboard/Keyboard.hpp"
+#include "cpu/Timer.hpp"
+#include "Object_manager.hpp"
 
 class Emulator
 {
 public:
-    Emulator()=default;
+    explicit Emulator(int w = 1024, int h = 768);
 
     void run();
     void inject_rom(const std::string& path, int start_index = 0x200);
 
 private:
 
+    void handle_events();
+    void render();
+    void update_graphics();
+
+    void emulate_cpu();
+    bool cpu_ready();
     std::vector<uint8_t> get_bytes(const std::string& path);
 
+    
+    sf::RenderWindow window;
+    Object_manager manager;
+
+    // in hz
+    int clock_speed = 20;
+
+    // keep track of bottlenecking
+    int clock_cycles_done = 0;
+
+    Timer timer;
     Memory ram;
-    CPU cpu{&ram};
+    Display display;
+    Keyboard kb;
+    CPU cpu{&ram, &kb, &display};
 };
 
 #endif // EMULATOR_HPP
