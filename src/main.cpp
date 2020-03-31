dc45a5d811afbf0fd01c37d3e94bb2d8c5e4b491
Jörg B <34109209+jbar98@users.noreply.github.com>
2020-03-31 19:26:21 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,18 @@
 #include "cpu/CPU.hpp"
 #include "memory/Memory.hpp"
+#include "Emulator.hpp"
 
 int main()
 {
     // http://devernay.free.fr/hacks/chip8/C8TECH10.HTM
     
-    Memory mem;
-    CPU cpu{&mem};
-    uint16_t test = 0x8FF6;
-    cpu.parse_instruction(test);
+    //Memory mem;
+    //CPU cpu{&mem};
+    //uint16_t test = 0xF065;
+    //cpu.parse_instruction(test);
 
+    Emulator emu;
+    emu.inject_rom("../roms/pong.rom");
+    emu.run();
     return 0;
 }
