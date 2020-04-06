fbef062838f21d5f8b09b91ad5b52037afa2933f
Jörg B <34109209+jbar98@users.noreply.github.com>
2020-04-07 01:28:53 +0200

    Minor changes

--- src/utility/Timer.hpp
+++ src/utility/Timer.hpp
@@ -2,6 +2,7 @@
 #define TIMER_HPP
 
 #include <chrono>
+#include <stdexcept>
 
 class Timer
 {
@@ -10,10 +11,11 @@ public:
     {
         cpu, 
         framerate,
-        seconds
+        seconds,
+        milliseconds
     };
 
-    bool is_ready(Type t, int required_rate)
+    bool is_ready(Type t, long long required_rate)
     {
         if (required_rate == 0)
             return false;
@@ -39,7 +41,7 @@ public:
             else
                 return false;
         }
-        else
+        else if (t==Type::seconds)
         {
             if (std::chrono::duration_cast<std::chrono::microseconds>(timer.now() - 
                 seconds).count() > (1'000'000 / required_rate))
@@ -50,9 +52,23 @@ public:
             else
                 return false;
         }
+        else if (t==Type::milliseconds)
+        {
+            if (std::chrono::duration_cast<std::chrono::milliseconds>(timer.now() - 
+                milliseconds).count() > (required_rate))
+            {
+                milliseconds = timer.now();
+                return true;
+            }
+            else
+                return false;
+        }
+
+        throw std::runtime_error{"unsupported timertype"};
     }
 
 private:
+    std::chrono::time_point<std::chrono::steady_clock> milliseconds{};
     std::chrono::time_point<std::chrono::steady_clock> seconds{};
     std::chrono::time_point<std::chrono::steady_clock> frame_interval{};
     std::chrono::time_point<std::chrono::steady_clock> cpu_interval{};
